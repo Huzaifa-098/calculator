055210bf2246e56cffe5147538d9596196e86908
Huzaifa Tariq <huzaifa603@gmail.com>
2022-08-31 11:54:49 +0500

    jS-BASIC

--- regex validation/valid.js
+++ regex validation/valid.js
@@ -1,98 +1,114 @@
-// var email = document.forms["form"]["email"];
-// var password = document.forms["form"]["password"];
-// var BusinessName = document.forms["form"]["BusinessName"];
+// var BusinessName1 = document.forms["form"]["BusinessName1"];
 
-// var email_error = document.getElementById("email_error");
-// var pass_error = document.getElementById("password_error");
 // var show_error = document.getElementById("show_error");
 
-// email.addEventListener("textInput", email_verify);
-// password.addEventListener("textInput", passwoed_verify);
-// BusinessName.addEventListener("textInput", BusinessName_verify);
+// var WellLocation1 = document.forms["form"]["WellLocation1"];
 
-// function validated() {
-//   if (email.value.length < 9) {
-//     email.style.border = "1px solid red";
-//     email_error.style.display = "block";
-//     email.focus();
-//     return false;
+// var WellLocation = document.getElementById("WellLocation");
+
+// var Blankbox1 = document.forms["form"]["Blankbox1"];
+// var Blankbox = document.getElementById("Blankbox");
+
+const form = document.querySelector("form");
+const CaseNo = document.getElementById("CaseNo");
+const LeaseNo = document.getElementById("LeaseNo");
+const CaseDate = document.getElementById("CaseDate");
+const CaseNoError = document.getElementById("caseError");
+const LeaseNoError = document.getElementById("leaseError");
+const CaseDateError = document.getElementById("CaseDateError");
+
+CaseNo.addEventListener("input", (event) => {
+  console.log("caseNO", CaseNo.validity.valid);
+  if (CaseNo.validity.valid) {
+    CaseNoError.textContent = "";
+    CaseNoError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+LeaseNo.addEventListener("input", (event) => {
+  console.log("LeaseNo", LeaseNo.validity.valid);
+  if (LeaseNo.validity.valid) {
+    LeaseNoError.textContent = "";
+    LeaseNoError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+CaseDate.addEventListener("input", (event) => {
+  console.log("caseDate", CaseDate.validity.valid);
+  if (CaseDate.validity.valid) {
+    CaseDateError.textContent = "";
+    CaseDateError.className = "error";
+  } else {
+    showError();
+  }
+});
+
+// LeaseNo.addEventListener("input", (event) => {
+//   console.log("LeaseNo", LeaseNo.validity.valid);
+//   if (LeaseNo.validity.valid) {
+//     LeaseNoError.textContent = "";
+//     LeaseNoError.className = "error";
+//   } else {
+//     showError();
 //   }
+// });
 
-//   if (password.value.length < 9) {
-//     password.style.border = "1px solid red";
-//     password_error.style.display = "block";
-//     password.focus();
-//     return false;
-//   }
-// }
-// function email_verify() {
-//   if (email.value.length >= 8) {
-//     email.style.border = "1px solid silver";
-//     email_error.style.display = "none";
-//     return false;
-//   }
-// }
-// function password_verify() {
-//   if (password.value.length >= 5) {
-//     password.style.border = "1px solid silver";
-//     password_error.style.display = "none";
-//     return false;
-//   }
-// }
-// if (BusinessName.value.length < 9) {
-//   BusinessName.style.border = "1px solid red";
-//   show_error.style.display = "none";
-//   BusinessName.focus();
-//   return false;
-// }
-
-// function BusinessName_verify() {
-//   if (BusinessName.value.length >= 8) {
-//     BusinessName.style.border = "1px solid silver";
-//     BusinessName_error.style.display = "none";
-//     return false;
-//   }
-// }
-
-var email = document.forms["form"]["email"];
-var password = document.forms["form"]["password"];
-var BusinessName = document.forms["form"]["BusinessName"];
-
-var email_error = document.getElementById("email_error");
-var pass_error = document.getElementById("password_error");
-var show_error = document.getElementById("show_error");
-
-// email.addEventListener("textInput", email_verify);
-// password.addEventListener("textInput", password_verify);
-
-function validated() {
-  if (email.value.length < 9) {
-    email.style.border = "1px solid red";
-    email_error.style.display = "block";
-    email.focus();
+function showError() {
+  if (CaseNo.validity.valueMissing) {
+    CaseNoError.textContent = "You need to enter an e-mail address.";
+  } else if (CaseNo.validity.typeMismatch) {
+    CaseNoError.textContent = "Entered value needs to be an e-mail address.";
+  } else if (CaseNo.validity.tooShort) {
+    CaseNoError.textContent = `Email should be at least ${CaseNo.minLength} characters; you entered ${CaseNo.value.length}.`;
   }
 
-  if (BusinessName.value.length < 9) {
-    BusinessName.style.border = "1px solid red";
-    show_error.style.display = "block";
+  if (LeaseNo.validity.valueMissing) {
+    LeaseNoError.textContent = "You need to enter an e-mail address.";
+  } else if (LeaseNo.validity.typeMismatch) {
+    LeaseNo.textContent = "Entered value needs to be an e-mail address.";
+  } else if (LeaseNo.validity.tooShort) {
+    LeaseNoError.textContent = `Email should be at least ${LeaseNo.minLength} characters; you entered ${LeaseNo.value.length}.`;
   }
-  if (password.value.length < 9) {
-    password.style.border = "1px solid red";
-    pass_error.style.display = "block";
+  if (CaseDate.validity.valueMissing) {
+    CaseDateError.textContent = "You need to enter an e-mail address.";
+  } else if (CaseDate.validity.typeMismatch) {
+    CaseDate.textContent = "Entered value needs to be an e-mail address.";
+  } else if (CaseDate.validity.tooShort) {
+    CaseDateError.textContent = `Email should be at least ${CaseDate.minLength} characters; you entered ${CaseDate.value.length}.`;
   }
-  return false;
+  CaseNoError.className = "error active";
+  LeaseNoError.className = "error active";
+  CaseDateError.className = "erroe active";
 }
-// function email_verify() {
-//   if (email.value.length >= 8) {
-//     email.style.border = "1px solid silver";
-//     email_error.style.display = "none";
-//     return false;
+
+// function showError() {
+//   if (LeaseNo.validity.valueMissing) {
+//     LeaseNoError.textContent = "You need to enter an e-mail address.";
+//   } else if (LeaseNo.validity.typeMismatch) {
+//     LeaseNo.textContent = "Entered value needs to be an e-mail address.";
+//   } else if (LeaseNo.validity.tooShort) {
+//     LeaseNoError.textContent = `Email should be at least ${LeaseNo.minLength} characters; you entered ${LeaseNo.value.length}.`;
 //   }
+
+//   LeaseNoError.className = "error active";
 // }
-// function password_verify() {
-//   if (password.value.length >= 5) {
-//     password.style.border = "1px solid silver";
-//     pass_error.style.display = "none";
-//     return false;
+
+form.addEventListener("submit", (event) => {
+  if (!CaseNo.validity.valid) {
+    showError();
+
+    event.preventDefault();
+  }
+});
+
+// form.addEventListener("submit", (event) => {
+//   if (!LeaseNo.validity.valid) {
+//     showError();
+
+//     event.preventDefault();
 //   }
-// }
+// });
